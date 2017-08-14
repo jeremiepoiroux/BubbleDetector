2f659e16fe9fb2e91c7bcdbc0da6c47b3b79610e
jeremiepoiroux <jeremie.poiroux@ens-lyon.fr>
2017-08-15 01:51:11 +0200

    node pie charts

--- bubbledetector_matplot.py
+++ bubbledetector_matplot.py
@@ -368,7 +368,7 @@ def plot_scatter(col, scale, title, t):
     plt.axis('off')
     if t != []:
         cbar = plt.colorbar(ticks=t)
-    plt.savefig("BTW17_" + title + '_scatter.png', dpi=200)
+    # plt.savefig("BTW17_" + title + '_scatter.png', dpi=200)
     # plt.show()
     # plt.draw()
     plt.close()
@@ -392,7 +392,7 @@ def plot_hist(data, colors, title, length):
     # plt.xlim(0, 897)
     # plt.ylim(0, 721)
     plt.autoscale(False)
-    plt.savefig("BTW17_" + title + '_histogram.png', dpi=96)
+    # plt.savefig("BTW17_" + title + '_histogram.png', dpi=96)
     # plt.show()
     plt.close()
 
@@ -452,8 +452,9 @@ def heatmap(xx, yy, ww, title):
     plt.axis('off')
     plt.autoscale(False)
     # plt.colorbar()
-    plt.savefig("BTW17_" + title + '_heatmap.png', dpi=200)
-    plt.show()
+    # plt.savefig("BTW17_" + title + '_heatmap.png', dpi=200)
+    #
+    # plt.show()
 
 heatmap(xs, ys, b1, "b1 network")
 heatmap(xs_0, ys_0, b1_0, "b1 community0")
@@ -511,8 +512,8 @@ for i in range(len(nodes_stats)):
         # plt.xlim(0, 897)
         # plt.ylim(0, 721)
         # plt.autoscale(False)
-        # plt.savefig("BTW17_" + title + '_histogram.png', dpi=96)
-        plt.show()
+        # plt.savefig("BTW17_" + title + str(z) + '_histogram.png', dpi=96)
+        # plt.show()
         plt.close()
 
     plot_hist_node(t, u, "#3F007D", "Nodes ")
@@ -521,9 +522,12 @@ for i in range(len(nodes_stats)):
 # for each node, find community for all nodes at distance 1
 
 list_friends_d1_new = []
+list_friends_d2_new = []
 for i in range(len(s["nodes"])):
     list_friends_d1_in = (s["nodes"][i]["id"], json.loads(s["nodes"][i]["list_of_friends_inside_community_at_distance_1"]))
     list_friends_d1_out = (s["nodes"][i]["id"], json.loads(s["nodes"][i]["list_of_friends_outside_community_at_distance_1"]))
+    list_friends_d2_in = (s["nodes"][i]["id"], json.loads(s["nodes"][i]["list_of_friends_inside_community_at_distance_2"]))
+    list_friends_d2_out = (s["nodes"][i]["id"], json.loads(s["nodes"][i]["list_of_friends_outside_community_at_distance_2"]))
     # print(list_friends_d1[1][0])
     # print(list_friends_d1_out)
     for j in list_friends_d1_in[1]:
@@ -534,7 +538,16 @@ for i in range(len(s["nodes"])):
         # print((i, j, s["nodes"][j]["community"]))
         list_friends_d1_new.append((i, j, s["nodes"][j]["community"]))
 
-list_friends_radar = []
+    for j in list_friends_d2_in[1]:
+        # print((i, j, s["nodes"][j]["community"]))
+        list_friends_d2_new.append((i, j, s["nodes"][j]["community"]))
+
+    for j in list_friends_d2_out[1]:
+        # print((i, j, s["nodes"][j]["community"]))
+        list_friends_d2_new.append((i, j, s["nodes"][j]["community"]))
+
+list_friends_radar_d1 = []
+list_friends_radar_d2 = []
 for i in range(len(s["nodes"])):
     def radar_node(k):
         l = []
@@ -545,90 +558,155 @@ for i in range(len(s["nodes"])):
                 l.append(n[2])
                 c = Counter(l)
                 m = (k, s["nodes"][k]["community"], c)
-        list_friends_radar.append(m)
+        list_friends_radar_d1.append(m)
 
     radar_node(i)
 
-list_friends_radar
+for r in range(len(s["nodes"])):
+    def radar_node(k):
+        l = []
+        m = []
+        for n in list_friends_d2_new:
+            if n[0] == k:
+                # print(i)
+                l.append(n[2])
+                c = Counter(l)
+                m = (k, s["nodes"][k]["community"], c)
+        list_friends_radar_d2.append(m)
 
-# radar for each node
+    radar_node(r)
+
+list_friends_radar_d1d2 = []
+for i in list_friends_radar_d1:
+    try:
+        j = i[0], i[1], i[2], list_friends_radar_d2[i[0]][2]
+        list_friends_radar_d1d2.append(j)
+    except:
+        TypeError
+
+list_friends_radar_d1
+list_friends_radar_d2
+list_friends_radar_d1d2
+
+# Donut distance 1
 
 zz = 0
-for i in list_friends_radar:
+for i in list_friends_radar_d1:
     try:
-        c0 = (i[0], i[1], i[2][0])
-        c1 = (i[0], i[1], i[2][1])
-        c2 = (i[0], i[1], i[2][2])
-        c3 = (i[0], i[1], i[2][3])
-        c4 = (i[0], i[1], i[2][4])
+        c0_1 = (i[0], i[1], i[2][0])
+        c1_1 = (i[0], i[1], i[2][1])
+        c2_1 = (i[0], i[1], i[2][2])
+        c3_1 = (i[0], i[1], i[2][3])
+        c4_1 = (i[0], i[1], i[2][4])
 
         # print(c0)
     except:
         TypeError
 
-    def plot_radar_node(a,b,c,d,e):
+    def plot_radar_node_d1(a,b,c,d,e,com):
 
-        # Set data
-        cat = ['0', '1', '2', '3', '4']
-        values = [a,b,c,d,e]
-        N = len(cat)
-        x_as = [n / float(N) * 2 * pi for n in range(N)]
+        # The slices will be ordered and plotted counter-clockwise.
+        labels = '0', '  1', '    2', '      3', '        4'
+        sizes = [a,b,c,d,e]
+        colors = ['#357AB1', '#B2C8E6', '#EF8637', '#F6BC80', '#57982C']
+        explode = (0, 0, 0, 0, 0)  # explode a slice if required
 
-        # Because our chart will be circular we need to append a copy of the first
-        # value of each list at the end of each list with data
-        values += values[:1]
-        x_as += x_as[:1]
+        plt.pie(sizes, explode=explode, labels=labels, colors=colors,autopct='%1.1f%%', shadow=False)
 
-        # Set color of axes
-        plt.rc('axes', linewidth=0.5, edgecolor="#888888")
+        #draw a circle at the center of pie to make it look like a donut
+        centre_circle = plt.Circle((0,0),0.75,color='black', fc='white',linewidth=0.25)
+        fig = plt.gcf()
+        fig.gca().add_artist(centre_circle)
 
-        # Create polar plot
-        ax = plt.subplot(111, polar=True)
-
-        # Set clockwise rotation. That is:
-        ax.set_theta_offset(pi / 2)
-        ax.set_theta_direction(-1)
-
-        # Set position of y-labels
-        ax.set_rlabel_position(0)
-
-        # Set color and linestyle of grid
-        ax.xaxis.grid(True, color="#888888", linestyle='solid', linewidth=0.5)
-        ax.yaxis.grid(True, color="#888888", linestyle='solid', linewidth=0.5)
-
-        # Set number of radial axes and remove labels
-        plt.xticks(x_as[:-1], [])
-
-        # Set yticks
-        plt.yticks([20, 40, 60, 80, 100], ["20", "40", "60", "80", "100"])
-
-        # Plot data
-        ax.plot(x_as, values, linewidth=0, linestyle='solid', zorder=3)
-
-        # Fill area
-        ax.fill(x_as, values, 'b', alpha=0.3)
-
-        # Set axes limits
-        plt.ylim(0, max(a,b,c,d,e))
-
-        # Draw ytick labels to make sure they fit properly
-        for i in range(N):
-            angle_rad = i / float(N) * 2 * pi
-
-            if angle_rad == 0:
-                ha, distance_ax = "center", 10
-            elif 0 < angle_rad < pi:
-                ha, distance_ax = "left", 1
-            elif angle_rad == pi:
-                ha, distance_ax = "center", 1
-            else:
-                ha, distance_ax = "right", 1
-
-        ax.text(angle_rad, max(a,b,c,d,e) + distance_ax, cat[i], size=10, horizontalalignment=ha, verticalalignment="center")
-        plt.title('#BTW17 - Community Radar Node ' + str(zz) + ' - \n ' + str(time.asctime()), x=0.5, y=1.2)
+        # Set aspect ratio to be equal so that pie is drawn as a circle.
+        plt.axis('equal')
+        plt.title('#BTW17 - Community Donut at d1 - Node ' + str(zz) + ' - Community ' + str(com) + ' - \n ' + str(time.asctime()), x=0.5, y=1.2)
+        plt.savefig("BTW17_Node" + str(zz) + '_donut_distance1.png', dpi=96)
         # Show polar plot
-        plt.show()
+        # plt.show()
         plt.close()
 
-    plot_radar_node(c0[2], c1[2], c2[2], c3[2], c4[2])
+    plot_radar_node_d1(c0_1[2], c1_1[2], c2_1[2], c3_1[2], c4_1[2], c0_1[1])
     zz = zz + 1
+
+# Donut distance 2
+
+zz = 0
+for i in list_friends_radar_d2:
+    try:
+        c0_1 = (i[0], i[1], i[2][0])
+        c1_1 = (i[0], i[1], i[2][1])
+        c2_1 = (i[0], i[1], i[2][2])
+        c3_1 = (i[0], i[1], i[2][3])
+        c4_1 = (i[0], i[1], i[2][4])
+
+        # print(c0)
+    except:
+        TypeError
+
+    def plot_radar_node_d2(a,b,c,d,e,com):
+
+        # The slices will be ordered and plotted counter-clockwise.
+        labels = '0', '  1', '    2', '      3', '        4'
+        sizes = [a,b,c,d,e]
+        colors = ['#357AB1', '#B2C8E6', '#EF8637', '#F6BC80', '#57982C']
+        explode = (0, 0, 0, 0, 0)  # explode a slice if required
+
+        plt.pie(sizes, explode=explode, labels=labels, colors=colors,autopct='%1.1f%%', shadow=False)
+
+        #draw a circle at the center of pie to make it look like a donut
+        centre_circle = plt.Circle((0,0),0.75,color='black', fc='white',linewidth=0.25)
+        fig = plt.gcf()
+        fig.gca().add_artist(centre_circle)
+
+        # Set aspect ratio to be equal so that pie is drawn as a circle.
+        plt.axis('equal')
+        plt.title('#BTW17 - Community Donut at d1 - Node ' + str(zz) + ' - Community ' + str(com) + ' - \n ' + str(time.asctime()), x=0.5, y=1.2)
+        # Show polar plot
+        # plt.savefig("BTW17_Node" + str(zz) + '_donut_distance2.png', dpi=96)
+        # plt.show()
+        plt.close()
+
+    plot_radar_node_d2(c0_1[2], c1_1[2], c2_1[2], c3_1[2], c4_1[2], c0_1[1])
+    zz = zz + 1
+
+# donuts d1 and d2
+
+for i in list_friends_radar_d1d2:
+    try:
+        c0_2 = (i[0], i[1], i[2][0], i[3][0])
+        c1_2 = (i[0], i[1], i[2][1], i[3][1])
+        c2_2 = (i[0], i[1], i[2][2], i[3][2])
+        c3_2 = (i[0], i[1], i[2][3], i[3][3])
+        c4_2 = (i[0], i[1], i[2][4], i[3][4])
+        # print(c0)
+    except:
+        TypeError
+
+    def pie(a,b,c,d,e,f,g,h,i,j,k,l):
+            # Group colors
+            com0 = '#357AB1'
+            com1 = '#B2C8E6'
+            com2 = '#EF8637'
+            com3 = '#F6BC80'
+            com4= '#57982C'
+
+            fig, ax = plt.subplots()
+            ax.axis('equal')
+            width = 0.3
+
+            # cm = plt.get_cmap("tab20c")
+            # cout = cm(np.arange(3)*4)
+            pie, _ = ax.pie([a,b,c,d,e], radius=1, colors=[com0, com1, com2, com3, com4])
+            plt.setp( pie, width=width, edgecolor='white')
+
+            # cin = cm(np.array([1,2,5,6,9,10]))
+            # labels = map("".join, zip(list("aabbcc"),map(str, [1,2]*3)))
+            pie2, _ = ax.pie([f,g,h,i,j], radius=1-width/2, colors=[com0, com1, com2, com3, com4])
+            plt.setp( pie2, width=width, edgecolor='white')
+            plt.title('#BTW17 - Community Donut at d1 and d2 - Node ' + str(l) + ' - Community ' + str(k) + ' - \n ' + str(time.asctime()), x=0.5, y=1.2)
+            # plt.savefig("BTW17_Node" + str(l) + '_donut_distance1and2.png', dpi=96)
+            # plt.show()
+            plt.close()
+
+    pie(c0_2[3],c1_2[3],c2_2[3],c3_2[3],c4_2[3], c0_2[2],c1_2[2],c2_2[2],c3_2[2],c4_2[2], c0_2[1], i[0])
